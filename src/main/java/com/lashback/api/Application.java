c93df78af3b01414bbd7e371cbc677cff94e820d
Danai Plantza <62573321+dplantza@users.noreply.github.com>
2026-05-09 09:03:21 +0300

    repo

--- src/main/java/com/lashback/api/Application.java
+++ src/main/java/com/lashback/api/Application.java
@@ -2,11 +2,17 @@ package com.lashback.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 /**
  * Main Spring Boot Application class for Lashback API
  */
-@SpringBootApplication(scanBasePackages = "com.lashback")
+@SpringBootApplication
+@EntityScan("com.lashback.model")
+@EnableJpaRepositories("com.lashback.repository")
+@ComponentScan("com.lashback")
 public class Application {
 
     public static void main(String[] args) {
